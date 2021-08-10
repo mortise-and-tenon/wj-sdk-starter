d7aefabb28dad2f601b1205ea7a587cf74cded2f
awu <softengineerwu@gmail.com>
2021-08-10 08:47:18 +0800

    初始化

---
+++ src/test/java/fun/mortnon/wj/MorTnonApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.mortnon.wj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorTnonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
